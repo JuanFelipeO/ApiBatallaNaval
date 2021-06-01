1b3fa6879f9bdefb065c42a76ef6206a78c4f9b7
Juan Felipe Ortiz <jfortiz87911@umanizales.edu.co>
2021-06-01 13:41:20 -0500

    Se elimina el contrador y el servicio del tablero y se cre la clase tablero

--- src/main/java/com/umanizales/apibatallanaval/model/Juego.java
+++ src/main/java/com/umanizales/apibatallanaval/model/Juego.java
@@ -1,18 +1,18 @@
 package com.umanizales.apibatallanaval.model;
 
-import com.umanizales.apibatallanaval.controller.TableroController;
 import com.umanizales.apibatallanaval.model.dto.DistribucionBarcoDTO;
+import com.umanizales.apibatallanaval.model.entities.Tablero;
 import com.umanizales.apibatallanaval.model.entities.Usuario;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Getter
 @Setter
-public class Juego {
+public class Juego
+{
     public int id;
-    public TableroController tableroJugador1;
-    public TableroController TableroJugador2;
+    public Tablero tableroJugador1;
+    public Tablero TableroJugador2;
     public int numeroBarcos; // preguntar cual es este tipo de dato ???
     public byte turno;
     public int aciertosJug1;
@@ -21,32 +21,11 @@ public class Juego {
     public ListaDE listaDE;
     public DistribucionBarcoDTO distribucionBarcoDTO;
 
-    public Juego(int id, TableroController tableroJugador1, TableroController tableroJugador2,
-                 int numeroBarcos, byte turno, int aciertosJug1, int aciertosJug2) {
-        this.id = id;
-        this.tableroJugador1 = tableroJugador1;
-        TableroJugador2 = tableroJugador2;
-        this.numeroBarcos = numeroBarcos;
-        this.turno = turno;
-        this.aciertosJug1 = aciertosJug1;
-        this.aciertosJug2 = aciertosJug2;
-    }
-
-    public void crearTableros() // terminar!!!
-    {
-        if (numeroBarcos > 0 && numeroBarcos <= 9)
-        {
-            // crear tabero aqui
-        }
-        if (numeroBarcos > 9 && numeroBarcos <= 20)
-        {
-            // crear tablero aqui
-        }
-        if (numeroBarcos > 20)
-        {
-            // crear tablero aqui
-        }
-    }
+   public Juego(int id, Usuario jugador1, Usuario jugador2, int numeroBarcos)
+   {
+       this.id = id;
+       this.numeroBarcos = numeroBarcos;
+   }
 
     public boolean disparar(int x, int y)
     {
